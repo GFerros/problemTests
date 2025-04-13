ff1645003a895e9a3763951b51b1d1a8b389a829
GFerros <gaferris1@gmail.com>
2025-04-13 15:30:54 -0400

    Implemented MyProblem -> DefaultValueTests to do verification testing for the Problem Interface

--- problem/src/test/java/org/myproblem/MyProblemTest.java
+++ problem/src/test/java/org/myproblem/MyProblemTest.java
@@ -6,6 +6,7 @@ import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,4 +19,37 @@ import static org.mockito.Mockito.when;
 
 public class MyProblemTest {
 
+    @Nested
+    class DefaultValueTests { //Test Case 1
+
+        @Test
+        public void MockValues() {
+            Problem problem = mock(Problem.class);
+            when(problem.getType()).thenReturn(URI.create("about:blank"));
+            when(problem.getTitle()).thenReturn(null);
+            when(problem.getStatus()).thenReturn(null);
+            when(problem.getDetail()).thenReturn(null);
+            when(problem.getInstance()).thenReturn(null);
+            when(problem.getParameters()).thenReturn(Collections.emptyMap());
+
+            assertEquals("about:blank", problem.getType().toString());
+            assertNull(problem.getTitle());
+            assertNull(problem.getStatus());
+            assertNull(problem.getDetail());
+            assertNull(problem.getInstance());
+            assertEquals(Collections.emptyMap(), problem.getParameters());
+
+        }
+
+        @Test
+        public void RealValues() {
+            Problem problem = Problem.builder().build();
+            assertEquals("about:blank", problem.getType().toString());
+            assertNull(problem.getTitle());
+            assertNull(problem.getStatus());
+            assertNull(problem.getDetail());
+            assertNull(problem.getInstance());
+            assertEquals(Collections.emptyMap(), problem.getParameters());
+        }
+    }
 }
