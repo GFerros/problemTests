6bd84aa3bb3446d3de5f2718bb75d7c44b9a4208
GFerros <gaferris1@gmail.com>
2025-04-13 15:52:15 -0400

    Implemented MyProblem -> ValueOf -> BasicInputTests to do verification testing for the ValueOf function for the Problem Interface

--- problem/src/test/java/org/myproblem/MyProblemTest.java
+++ problem/src/test/java/org/myproblem/MyProblemTest.java
@@ -52,4 +52,80 @@ public class MyProblemTest {
             assertEquals(Collections.emptyMap(), problem.getParameters());
         }
     }
+
+    @Nested
+    class ValueOfTests { //Test Case 2-4
+
+        @Nested
+        class BasicInputTests { //Test Case 2
+
+            @Test
+            void withStatusOnly() { //valueOf with status only as parameter
+                Problem problem = Problem.valueOf(Status.NOT_FOUND);
+
+                assertEquals(404, Objects.requireNonNull(problem.getStatus()).getStatusCode());
+                //Note that the title is set to the status's reason phrase automatically
+                assertEquals("Not Found", problem.getTitle());
+                assertNull(problem.getDetail());
+                assertNull(problem.getInstance());
+                assertEquals(Problem.DEFAULT_TYPE, problem.getType());
+                assertEquals(Collections.emptyMap(), problem.getParameters());
+            }
+
+            @Test
+            void withStatusAndDetail() { //valueOf with status and detail as parameters
+                Problem problem = Problem.valueOf(Status.NOT_FOUND, "Order 123");
+
+                assertEquals(404, Objects.requireNonNull(problem.getStatus()).getStatusCode());
+                assertEquals("Order 123", problem.getDetail());
+                assertNull(problem.getInstance());
+                assertEquals(Problem.DEFAULT_TYPE, problem.getType());
+                assertEquals(Collections.emptyMap(), problem.getParameters());
+            }
+
+            @Test
+            void withStatusAndInstance() {
+                URI instance = URI.create("https://example.org/orders/123");
+                Problem problem = Problem.valueOf(Status.NOT_FOUND, instance);
+
+                assertEquals(404, Objects.requireNonNull(problem.getStatus()).getStatusCode());
+                assertNull(problem.getDetail());
+                assertEquals(instance, problem.getInstance());
+                assertEquals(Problem.DEFAULT_TYPE, problem.getType());
+                assertEquals(Collections.emptyMap(), problem.getParameters());
+            }
+
+
+            @Test
+            void withStatusDetailAndInstance() {
+                URI instance = URI.create("https://example.org/orders/123");
+                Problem problem = Problem.valueOf(Status.NOT_FOUND, "Order 123", instance);
+
+                assertEquals(404, Objects.requireNonNull(problem.getStatus()).getStatusCode());
+                assertEquals("Order 123", problem.getDetail());
+                assertEquals(instance, problem.getInstance());
+                assertEquals(Problem.DEFAULT_TYPE, problem.getType());
+                assertEquals(Collections.emptyMap(), problem.getParameters());
+            }
+
+
+            @Nested
+            class InvalidInputTests {
+
+                @Test
+                void shouldHandleInvalidURI() {
+                    assertThrows(IllegalArgumentException.class, () ->
+                            Problem.valueOf(Status.NOT_FOUND, URI.create("invalid uri")));
+                }
+
+                @Test
+                void shouldHandleMalformedURI() {
+                    assertThrows(IllegalArgumentException.class, () ->
+                            Problem.valueOf(Status.NOT_FOUND, URI.create("http://example.org/{invalid}")));
+                }
+
+
+            }
+        }
+    }
 }
